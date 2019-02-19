a006f071b0c719859b9900b23a973e2dca340773
roll <eskarev@gmail.com>
2019-02-19 11:55:34 +0300

    Removed questinable required fields from deposited-dataset

--- scripts/generate_deposited_dataset_schema.py
+++ scripts/generate_deposited_dataset_schema.py
@@ -18,12 +18,12 @@ def generate_deposited_dataset_schema():
 
     # Remove dataset required flags
     for field in schema['dataset_fields'] + schema['resource_fields']:
-        if field['field_name'] not in ['title', 'geog_coverage']:
+        if field['field_name'] not in ['title']:
             field['required'] = False
 
     # Remove resource required flags
     for field in schema['resource_fields'] + schema['resource_fields']:
-        if field['field_name'] not in ['type', 'date_range_start', 'date_range_end']:
+        if field['field_name'] not in ['type']:
             field['required'] = False
 
     # Handle organization fields
