51f5d4940f053454efaee15d4fc47ab4e5f3b32a
roll <eskarev@gmail.com>
2019-02-20 18:45:43 +0300

    Fixed approve/reject buttons for curators

--- ckanext/unhcr/controllers/deposited_dataset.py
+++ ckanext/unhcr/controllers/deposited_dataset.py
@@ -10,13 +10,16 @@ from ckanext.unhcr import helpers
 log = logging.getLogger(__name__)
 
 
+# Module API
+
 class DepositedDatasetController(toolkit.BaseController):
 
     def approve(self, id):
 
-        # Check access and get dataset
-        _raise_not_authz_or_not_deposited(id)
-        pkg_dict = get_core.package_show({'model': model}, {'id': id})
+        # Get access and check authz
+        context = _get_no_authz_context()
+        pkg_dict = get_core.package_show(context, {'id': id})
+        _raise_not_authz_or_not_deposited(pkg_dict)
 
         # Change to regural dataset
         pkg_dict = helpers.convert_deposited_dataset_to_regular_dataset(pkg_dict)
@@ -24,8 +27,8 @@ class DepositedDatasetController(toolkit.BaseController):
         # Update package
         # TODO: Validation errors are impossible here for normal flow
         # We also set type in context to allow type switching by ckan patch
-        context = {'model': model, 'user': toolkit.c.user, 'type': pkg_dict['type']}
-        pkg_dict = update_core.package_update(context, pkg_dict)
+        update_context = _get_no_authz_context(type=pkg_dict['type'])
+        pkg_dict = update_core.package_update(update_context, pkg_dict)
 
         # Send approval email
         #  mail_data_container_update_to_user({}, pkg_dict, event='approval')
@@ -37,31 +40,41 @@ class DepositedDatasetController(toolkit.BaseController):
     def reject(self, id, *args, **kwargs):
 
         # Check access and get dataset
-        _raise_not_authz_or_not_deposited(id)
-        pkg_dict = get_core.package_show({'model': model}, {'id': id})
+        context = _get_no_authz_context()
+        pkg_dict = get_core.package_show(context, {'id': id})
+        _raise_not_authz_or_not_deposited(pkg_dict)
 
         # Send rejection email
         #  mail_data_container_update_to_user({}, pkg_dict, event='rejection')
 
         # Purge rejected dataset
-        delete_core.dataset_purge({'model': model}, {'id': id})
+        delete_core.dataset_purge(context, {'id': id})
 
         # Show flash message and redirect
         toolkit.h.flash_error('Datasest "{}" is rejected'.format(pkg_dict['title']))
         toolkit.redirect_to('data-container_index')
 
 
-def _raise_not_authz_or_not_deposited(id):
+# Internal
+
+def _get_no_authz_context(**patch):
+    context = {'model': model, 'user': toolkit.c.user, 'ignore_auth': True}
+    context.update(patch)
+    return context
+
+
+def _raise_not_authz_or_not_deposited(pkg_dict):
     depo = helpers.get_data_container_for_depositing()
 
     # Check dataset exists and it's deposited
-    pkg_dict = get_core.package_show({'model': model}, {'id': id})
-    if pkg_dict.get('owner_org') == depo['id']:
-        raise toolkit.ObjectNotFound('Deposited dataset "%s" not found' % id)
+    if pkg_dict.get('owner_org') != depo['id']:
+        message = 'Deposited dataset "%s" not found' % pkg_dict['id']
+        raise toolkit.ObjectNotFound(message)
 
     # Check the user is sysadmin or data curator
     granted = lib_helpers.check_access('sysadmin')
     if not granted:
         granted = lib_helpers.user_in_org_or_group(depo['id'])
     if not granted:
-        raise toolkit.ObjectNotFound('Deposited dataset "%s" is forbidden' % id)
+        message = 'Deposited dataset "%s" is forbidden' % pkg_dict['id']
+        raise toolkit.ObjectNotFound(message)
